a372092446b9c5d36be698191cb920d8cbfbe1ff
Jonathan Rockway <jon@jrock.us>
2010-09-12 03:41:35 -0500

    make calling the callback a method, so that modifiers can modify it

--- lib/AnyEvent/Inotify/EventReceiver/Callback.pm
+++ lib/AnyEvent/Inotify/EventReceiver/Callback.pm
@@ -5,15 +5,19 @@ use Moose;
 use MooseX::Types::Moose qw(CodeRef);
 
 has 'callback' => (
+    traits   => ['Code'],
     is       => 'ro',
     isa      => CodeRef,
     required => 1,
+    handles  => {
+        call_callback => 'execute',
+    },
 );
 
 for my $event (qw/access modify attribute_change close open move delete create/){
     __PACKAGE__->meta->add_method( "handle_$event" => sub {
         my $self = shift;
-        $self->callback->($event, @_);
+        $self->call_callback($event, @_);
     });
 }
 
